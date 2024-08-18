1b75bd48abdcd01f3db70fe436a245ea6edd2b13
ozodbec <ozodbekmirzaaxmadov05@gmail.com>
2024-08-18 20:08:54 +0500

    Comfy Store

---
+++ src/pages/Error.jsx
@@ -0,0 +1,36 @@
+import { useRouteError, Link } from "react-router-dom";
+
+const Error = () => {
+  const error = useRouteError();
+  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <main className="grid min-h-[100vh] place-items-center px-8">
+        <div className="text-center">
+          <p className="text-9xl font-semibold text-black ">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            Page Not Found
+          </h1>
+          <p className="mt-6 text-lg leading-7">
+            Kechirasiz, siz qidirayotgan sahifani topa olmadik :(
+          </p>
+          <div className="mt-10">
+            <Link to="/" className="btn bg-black text-white hover:bg-slate-700">
+              go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main className="grid min-h-[100vh] place-items-center px-8">
+      <h4 className="text-center font-bold text-4xl">
+        Xatolik yuz berdi... E'tiborli bo'ling!
+      </h4>
+    </main>
+  );
+};
+export default Error;
